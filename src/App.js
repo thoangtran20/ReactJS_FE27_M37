0979f34f8401eb550d0bf3e9f8f3896da0ab033a
thoangtran20 <77189736+thoangtran20@users.noreply.github.com>
2022-09-08 22:25:25 +0700

    Homework 1 - FE27 - M37

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { CardItem } from './components/CardItem';
+
+function App() {
+  return (
+    <div className="App">
+      <CardItem></CardItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardItem.jsx
@@ -0,0 +1,36 @@
+import './style.css'
+import img from '../components/images/images.png'
+
+export const CardItem = () => {
+  return (
+    <div className="card-item">
+      <>
+      <div className="card-item-header">
+        <img src={img} alt="" />
+      </div>
+     </>
+      <div className="card-item-body">
+        <div className="body-top">
+          <h2 className="top-header">Title</h2>
+          <span>* * * * * 4.5 (413)</span>
+          <h5 className="top-text">Secondary text</h5>
+          <p className="top-description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor
+          </p>
+        </div>
+        <hr />
+        <div className="body-bottom">
+          <h2 className="bottom-header">Subtitle</h2>
+          <div className="bottom-item">
+            <button className="bottom-btn">Item 1</button>
+            <button className="bottom-btn">Item 2</button>
+            <button className="bottom-btn">Item 3</button>
+            <button className="bottom-btn">Item 4</button>
+          </div>
+          <a href="$" className="bottom-link">ACTION 1</a>
+        </div>
+      </div>
+    </div>
+  )
+}
